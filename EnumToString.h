858d56c1eef1757df1101eb36248874b55027c6d
Éric Jacopin <ejacopin@ymail.com>
2021-02-16 22:05:04 +0100

    Simplified enum forward declaration

--- EnumToString.h
+++ EnumToString.h
@@ -1,8 +1,8 @@
 // ================================================================================================ File name: "EnumToString.h"
 // Adapted from https://www.codeproject.com/Articles/10500/Converting-C-enums-to-strings
 //    - Added "class" line 12
-//    - Added "const" line 13, 16 and 17.
-//    - Added "enum tag##ENUM_NAME : short ;" line 16
+//    - Added "const" line 13, and 17.
+//    - Added "enum tag##ENUM_NAME ; const" line 16
 #undef DECL_ENUM_ELEMENT
 #undef BEGIN_ENUM
 #undef END_ENUM
@@ -13,6 +13,6 @@
 #define END_ENUM( ENUM_NAME ) ENUM_NAME; const char* GetString##ENUM_NAME(enum tag##ENUM_NAME index);
 #else
 #define DECL_ENUM_ELEMENT( element ) #element
-#define BEGIN_ENUM( ENUM_NAME ) enum tag##ENUM_NAME : short ; const char* gs_##ENUM_NAME [] =
+#define BEGIN_ENUM( ENUM_NAME ) enum tag##ENUM_NAME ; const char* gs_##ENUM_NAME [] =
 #define END_ENUM( ENUM_NAME ) ; const char* GetString##ENUM_NAME(enum tag##ENUM_NAME index){ return gs_##ENUM_NAME [index]; }
 #endif 
