6b5b4a6bff35ca109a6c1559a32dbb48db59bff8
Éric Jacopin <ejacopin@ymail.com>
2021-02-16 21:09:02 +0100

    fixed ENUM forward declaration

--- EnumToString.h
+++ EnumToString.h
@@ -1,20 +1,18 @@
 // ================================================================================================ File name: "EnumToString.h"
 // Adapted from https://www.codeproject.com/Articles/10500/Converting-C-enums-to-strings
-//    - Added class line 12
-//    - Added const line 14, 17 and 18.
-//    - Removed enum line 14
+//    - Added "class" line 12
+//    - Added "const" line 13, 16 and 17.
+//    - Added "enum tag##ENUM_NAME : short ;" line 16
 #undef DECL_ENUM_ELEMENT
 #undef BEGIN_ENUM
 #undef END_ENUM
 
 #ifndef GENERATE_ENUM_STRINGS
 #define DECL_ENUM_ELEMENT( element ) element
-#define BEGIN_ENUM( ENUM_NAME ) typedef enum class tag##ENUM_NAME
-#define END_ENUM( ENUM_NAME ) ENUM_NAME; \
-            const char* GetString##ENUM_NAME(##ENUM_NAME index);
+#define BEGIN_ENUM( ENUM_NAME ) typedef enum class tag##ENUM_NAME 
+#define END_ENUM( ENUM_NAME ) ENUM_NAME; const char* GetString##ENUM_NAME(enum tag##ENUM_NAME index);
 #else
 #define DECL_ENUM_ELEMENT( element ) #element
-#define BEGIN_ENUM( ENUM_NAME ) const char* gs_##ENUM_NAME [] =
-#define END_ENUM( ENUM_NAME ) ; const char* GetString##ENUM_NAME(enum \
-            tag##ENUM_NAME index){ return gs_##ENUM_NAME [index]; }
-#endif
+#define BEGIN_ENUM( ENUM_NAME ) enum tag##ENUM_NAME : short ; const char* gs_##ENUM_NAME [] =
+#define END_ENUM( ENUM_NAME ) ; const char* GetString##ENUM_NAME(enum tag##ENUM_NAME index){ return gs_##ENUM_NAME [index]; }
+#endif 
